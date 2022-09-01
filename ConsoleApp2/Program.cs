e142f4a55bbc2e38254bdfbe24107957542dba5d
scott <scott@DESKTOP-4O9J4JT>
2022-09-01 10:51:00 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tast inn en temperatur, et heltall");
+            int grader = Convert.ToInt32(Console.ReadLine());
+            
+            if (grader < 0)
+            {
+                Console.WriteLine("minusgrader");
+            }
+
+            else
+            {
+                Console.WriteLine("plussgrader");
+            }
+        }
+    }
+}
